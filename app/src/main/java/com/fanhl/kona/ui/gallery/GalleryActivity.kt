e0453c8989776cc1b1c11f2752d96c119b945940
fanhl <fanhonglin@klicen.com>
2018-04-24 16:37:41 +0800

    全屏

--- app/src/main/java/com/fanhl/kona/ui/gallery/GalleryActivity.kt
+++ app/src/main/java/com/fanhl/kona/ui/gallery/GalleryActivity.kt
@@ -2,6 +2,8 @@ package com.fanhl.kona.ui.gallery
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
@@ -56,6 +58,11 @@ class GalleryActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        window.statusBarColor = Color.TRANSPARENT
+        window.navigationBarColor = Color.TRANSPARENT
+
         setContentView(R.layout.activity_gallery)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
