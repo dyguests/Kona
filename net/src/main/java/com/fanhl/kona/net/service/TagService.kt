b677f4f1b83d2cbec41b911c12bb1d4711857cd5
fanhl <dyguests@qq.com>
2018-04-28 23:03:43 +0800

    tag

---
+++ net/src/main/java/com/fanhl/kona/net/service/TagService.kt
@@ -0,0 +1,16 @@
+package com.fanhl.kona.net.service
+
+import com.fanhl.kona.net.model.Post
+import com.fanhl.kona.net.model.Tag
+import io.reactivex.Observable
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface TagService {
+    @GET("/tag.json")
+    fun getTag(
+            @Query("tags") tags: String? = null,
+            @Query("page") page: Int? = null,
+            @Query("limit") limit: Int? = null
+    ): Observable<List<Tag>>
+}
