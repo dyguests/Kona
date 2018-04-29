35cbeeb3a93711e7295a160586437f4103f616c9
fanhl <dyguests@qq.com>
2018-04-29 17:44:49 +0800

    AppDatabase

--- db/src/main/java/com/fanhl/kona/db/AppDatabase.kt
+++ db/src/main/java/com/fanhl/kona/db/AppDatabase.kt
@@ -1,4 +1,10 @@
 package com.fanhl.kona.db
 
-abstract class AppDatabase {
+import android.arch.persistence.room.Database
+import android.arch.persistence.room.RoomDatabase
+import com.fanhl.kona.model.Tag
+
+@Database(entities = [Tag::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun tagDao(): TagDao
 }
