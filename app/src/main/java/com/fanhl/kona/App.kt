066ea2e6fceaa293f49709a511562695931edeb4
fanhl <dyguests@qq.com>
2018-04-29 17:53:16 +0800

    db

--- app/src/main/java/com/fanhl/kona/App.kt
+++ app/src/main/java/com/fanhl/kona/App.kt
@@ -1,15 +1,14 @@
 package com.fanhl.kona
 
 import android.app.Application
+import com.fanhl.kona.db.KonaDatabase
 import com.fanhl.kona.net.KonaClient
 import com.fanhl.util.BlankjUtils
-import com.fanhl.kona.db.AppDatabase
-import android.arch.persistence.room.Room
 
 
 class App : Application() {
     val client by lazy { KonaClient() }
-    var db = Room.databaseBuilder(applicationContext, AppDatabase::class.java, "database-name").build()
+    val db by lazy { KonaDatabase.create(this) }
 
     override fun onCreate() {
         super.onCreate()
---
+++ db/src/main/java/com/fanhl/kona/db/KonaDatabase.kt
@@ -0,0 +1,19 @@
+package com.fanhl.kona.db
+
+import android.arch.persistence.room.Database
+import android.arch.persistence.room.Room
+import android.arch.persistence.room.RoomDatabase
+import android.content.Context
+import com.fanhl.kona.db.dao.TagDao
+import com.fanhl.kona.model.Tag
+
+@Database(entities = [Tag::class], version = 1)
+abstract class KonaDatabase : RoomDatabase() {
+    abstract fun tagDao(): TagDao
+
+    companion object {
+        const val DATABASE_NAME = "kona"
+
+        fun create(applicationContext: Context) = Room.databaseBuilder(applicationContext, KonaDatabase::class.java, KonaDatabase.DATABASE_NAME).build()
+    }
+}
