99679785f1fef2220670f579dd6bc0877db689ba
fanhl <dyguests@qq.com>
2018-04-30 21:06:54 +0800

    main liveData

--- app/src/main/java/com/fanhl/kona/ui/main/MainActivity.kt
+++ app/src/main/java/com/fanhl/kona/ui/main/MainActivity.kt
@@ -5,6 +5,7 @@ import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModelProviders
 import android.content.Intent
 import android.os.Bundle
+import android.support.v7.util.DiffUtil
 import android.view.Menu
 import android.view.MenuItem
 import android.view.inputmethod.EditorInfo
@@ -140,6 +141,27 @@ class MainActivity : BaseActivity() {
 //        }
 
         viewModel.tag.observe(this) { tv_tags.setText(it) }
+        viewModel.posts.observe(this) {
+            DiffUtil
+                    .calculateDiff(object : DiffUtil.Callback() {
+                        override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int) = adapter.data[oldItemPosition].id == it?.get(newItemPosition)?.id ?: -1
+
+                        override fun getOldListSize() = adapter.data.size
+
+                        override fun getNewListSize() = it?.size ?: 0
+
+                        override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+                            val oldPost = adapter.data[oldItemPosition]
+                            val newPost = it?.get(newItemPosition)
+                            return newPost != null
+                                    && oldPost.previewUrl == newPost.previewUrl
+                                    && oldPost.width == newPost.width
+                                    && oldPost.height == newPost.height
+                        }
+                    })
+                    .dispatchUpdatesTo(adapter)
+            adapter.setNewData(it)
+        }
     }
 
     private fun initData() {
@@ -181,9 +203,9 @@ class MainActivity : BaseActivity() {
                 .subscribeBy(
                         onNext = {
                             if (loadMore) {
-                                adapter.addData(it)
+                                viewModel.posts.value = it
                             } else {
-                                adapter.setNewData(it)
+                                viewModel.posts.value = viewModel.posts.value.orEmpty().toMutableList().apply { addAll(it) }
                             }
                             adapter.loadMoreComplete()
 
@@ -231,6 +253,6 @@ class MainActivity : BaseActivity() {
     class ViewModel : android.arch.lifecycle.ViewModel() {
         val tag by lazy { MutableLiveData<String>() }
         val page by lazy { MutableLiveData<Int>() }
-//        val posts by lazy { MutableLiveData<List<Post>>() }
+        val posts by lazy { MutableLiveData<List<Post>>() }
     }
 }
--- app/src/main/java/com/fanhl/kona/util/live_data.kt
+++ app/src/main/java/com/fanhl/kona/util/live_data.kt
@@ -1,7 +1,8 @@
 package com.fanhl.kona.util
 
+import android.arch.lifecycle.LifecycleOwner
 import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.Observer
 import com.fanhl.kona.ui.main.MainActivity
 
-fun <T> LiveData<T>.observe(mainActivity: MainActivity, observer: (T?) -> Unit) = observe(mainActivity, Observer<T> { observer(it) })
+fun <T> LiveData<T>.observe(owner: LifecycleOwner, observer: (T?) -> Unit) = observe(owner, Observer<T> { observer(it) })
