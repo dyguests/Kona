9fa28522fcc2796a8b1271377b10be90af3176c2
fanhl <dyguests@qq.com>
2018-04-30 10:10:14 +0800

    快搞定了

--- model/src/main/java/com/fanhl/kona/model/Tag.kt
+++ model/src/main/java/com/fanhl/kona/model/Tag.kt
@@ -1,14 +1,16 @@
 package com.fanhl.kona.model
 
+import android.arch.persistence.room.ColumnInfo
 import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
 
 @Entity
 data class Tag(
-        var id: Int? = null,
-        var name: String?,
-        var count: Int? = null,
-        var type: Int? = null,
-        var ambiguous: Boolean? = null
+        @PrimaryKey var id: Long? = null,
+        @ColumnInfo(name = "name") var name: String?,
+        @ColumnInfo(name = "count") var count: Int? = null,
+        @ColumnInfo(name = "type") var type: Int? = null,
+        @ColumnInfo(name = "ambiguous") var ambiguous: Boolean? = null
 ) {
-    constructor() : this(name = "")
+    constructor() : this(System.currentTimeMillis(), "name", 0, 0, false)
 }
