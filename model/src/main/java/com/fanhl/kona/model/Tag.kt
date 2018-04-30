677d4acbde2a58ecdfc794a42cdf217236549e9b
fanhl <dyguests@qq.com>
2018-04-30 17:24:09 +0800

    tag

--- model/src/main/java/com/fanhl/kona/model/Tag.kt
+++ model/src/main/java/com/fanhl/kona/model/Tag.kt
@@ -6,7 +6,7 @@ import android.arch.persistence.room.PrimaryKey
 @Entity
 data class Tag(
         var id: Long? = null,
-        @PrimaryKey var name: String?,
+        @PrimaryKey var name: String,
         var count: Int? = null,
         var type: Int? = null,
         var ambiguous: Boolean? = null
