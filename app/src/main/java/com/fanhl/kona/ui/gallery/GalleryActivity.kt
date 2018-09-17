8159efbcc1d8e145a5a3a06500af7440ad6fc33e
fanhl <fanhonglin@klicen.com>
2018-09-17 17:31:33 +0800

    RequestListener<Drawable>

--- app/src/main/java/com/fanhl/kona/ui/gallery/GalleryActivity.kt
+++ app/src/main/java/com/fanhl/kona/ui/gallery/GalleryActivity.kt
@@ -6,6 +6,7 @@ import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModelProviders
 import android.content.Intent
 import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
 import android.os.Build
 import android.os.Bundle
 import android.support.constraint.ConstraintSet
@@ -13,7 +14,11 @@ import android.support.transition.TransitionManager
 import android.support.v4.content.ContextCompat
 import android.view.View
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.Target
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.BaseViewHolder
 import com.fanhl.kona.R
@@ -132,6 +137,16 @@ class GalleryActivity : BaseActivity() {
 //                    .load("http://img0.imgtn.bdimg.com/it/u=4236860535,1526027473&fm=27&gp=0.jpg")
                     .thumbnail(.1f)
                     .apply(RequestOptions().dontTransform())
+                    .listener(object : RequestListener<Drawable> {
+                        override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Drawable>?, isFirstResource: Boolean): Boolean {
+                            return false
+                        }
+
+                        override fun onResourceReady(resource: Drawable?, model: Any?, target: Target<Drawable>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
+                            resource
+                            return false
+                        }
+                    })
                     .into(photo_view)
 
             recycler_view.adapter = tagAdapter
---
+++ app/src/main/java/com/fanhl/kona/util/FileUtils.kt
@@ -0,0 +1,4 @@
+package com.fanhl.kona.util
+
+object FileUtils {
+}
