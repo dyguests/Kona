e30ac2f20d3a3c4912fe16bde7f5121f8073b0e5
fanhl <dyguests@qq.com>
2019-03-25 00:49:07 +0800

    androidX init.

--- app/src/main/java/com/fanhl/kona/util/app.kt
+++
@@ -1,9 +0,0 @@
-package com.fanhl.kona.util
-
-import android.view.View
-import com.fanhl.kona.App
-
-val View.app: App
-    get() {
-        return context.applicationContext as App
-    }
--- app/src/main/java/com/fanhl/kona/util/live_data.kt
+++
@@ -1,8 +0,0 @@
-package com.fanhl.kona.util
-
-import android.arch.lifecycle.LifecycleOwner
-import android.arch.lifecycle.LiveData
-import android.arch.lifecycle.Observer
-import com.fanhl.kona.ui.main.MainActivity
-
-fun <T> LiveData<T>.observe(owner: LifecycleOwner, observer: (T?) -> Unit) = observe(owner, Observer<T> { observer(it) })
