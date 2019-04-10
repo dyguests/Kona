596a0337d231c301710232be5a9f970340a5b4c6
fanhl <dyguests@qq.com>
2019-04-10 19:44:18 +0800

    ViewModel

--- app/src/main/java/com/fanhl/kona/base/di/DaggerViewModelFactory.kt
+++ app/src/main/java/com/fanhl/kona/base/di/DaggerViewModelFactory.kt
@@ -6,10 +6,18 @@ import javax.inject.Inject
 import javax.inject.Provider
 
 class DaggerViewModelFactory @Inject constructor(
-    private val creator: Map<Class<out ViewModel>, @JvmSuppressWildcards Provider<ViewModel>>
+    private val creators: Map<Class<out ViewModel>, @JvmSuppressWildcards Provider<ViewModel>>
 ) : ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val creator = creators[modelClass] ?: creators.entries.firstOrNull {
+            modelClass.isAssignableFrom(it.key)
+        }?.value
+        ?: throw IllegalArgumentException("Can't find ViewModel to inject ($modelClass) - perhaps it's not added to ViewModelModule?")
+        try {
+            @Suppress("UNCHECKED_CAST")
+            return creator.get() as T
+        } catch (e: ClassCastException) {
+            throw IllegalStateException("Something went terribly wrong while instantiating a ViewModel", e)
+        }
     }
-
 }
--- app/src/main/java/com/fanhl/kona/base/di/ViewModelModule.kt
+++ app/src/main/java/com/fanhl/kona/base/di/ViewModelModule.kt
@@ -1,8 +1,10 @@
 package com.fanhl.kona.base.di
 
 import androidx.lifecycle.ViewModelProvider
+import com.fanhl.kona.di.ViewModelKey
 import dagger.Binds
 import dagger.Module
+import dagger.multibindings.IntoMap
 import javax.inject.Singleton
 
 @Module
@@ -10,4 +12,9 @@ abstract class ViewModelModule {
     @Binds
     @Singleton
     abstract fun bindViewModelFactory(daggerViewModelFactory: DaggerViewModelFactory): ViewModelProvider.Factory
+
+//    @Binds
+//    @IntoMap
+//    @ViewModelKey(JobListViewModel::class)
+//    abstract fun bindListViewModel(jobListViewModel: JobListViewModel): ViewModel
 }
