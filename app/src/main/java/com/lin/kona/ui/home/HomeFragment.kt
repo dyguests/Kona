36e404fa460a8245f3d619c41560458addfbc1f6
fanhl <dyguests@gmail.com>
2023-03-11 21:00:51 +0800

    viewModel.

--- app/src/main/java/com/lin/kona/ui/home/HomeFragment.kt
+++ app/src/main/java/com/lin/kona/ui/home/HomeFragment.kt
@@ -4,23 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.lin.kona.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+    private val viewModel by lazy { ViewModelProvider(this)[HomeViewModel::class.java] }
 
     private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
-        val homeViewModel = ViewModelProvider(this)[HomeViewModel::class.java]
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
