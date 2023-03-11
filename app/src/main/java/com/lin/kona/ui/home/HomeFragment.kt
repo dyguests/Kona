1dfe1165723dd6fc2b33a429351592199404aed0
fanhl <dyguests@gmail.com>
2023-03-11 20:38:31 +0800

    cover placeholder.

--- app/src/main/java/com/lin/kona/ui/home/HomeFragment.kt
+++ app/src/main/java/com/lin/kona/ui/home/HomeFragment.kt
@@ -18,20 +18,17 @@ class HomeFragment : Fragment() {
     private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+        val homeViewModel = ViewModelProvider(this)[HomeViewModel::class.java]
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        // val textView: TextView = binding.textHome
+        // homeViewModel.text.observe(viewLifecycleOwner) {
+        //     textView.text = it
+        // }
         return root
     }
 
