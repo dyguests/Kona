2172dd4119f0ebd975d77e020c1a97fa7c243862
fanhl <dyguests@gmail.com>
2023-03-12 18:02:32 +0800

    CoroutineTest.

---
+++ app/src/test/java/com/lin/kona/coroutine/Coroutine2Test.kt
@@ -0,0 +1,64 @@
+package com.lin.kona.coroutine
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import org.junit.Test
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class Coroutine2Test {
+
+    @Test
+    fun testFun0() {
+        GlobalScope.launch {
+            println(111)
+            launch {
+                println(222)
+            }
+            println(333)
+            launch {
+                println(444)
+            }
+            println(555)
+        }
+    }
+
+    @Test
+    fun testFun() {
+        GlobalScope.launch {
+            println(111)
+            delay(1000)
+            launch {
+                println(222)
+            }
+            delay(1000)
+            println(333)
+            delay(1000)
+            launch {
+                println(444)
+            }
+            delay(1000)
+            println(555)
+        }
+    }
+
+    @Test
+    fun testFun2() {
+        GlobalScope.launch(Dispatchers.Default) {
+            println(111)
+            launch(Dispatchers.IO) {
+                println(222)
+            }
+            println(333)
+            launch(Dispatchers.Main) {
+                println(444)
+            }
+            println(555)
+        }
+    }
+}
---
+++ app/src/test/java/com/lin/kona/coroutine/CoroutineTest.kt
@@ -0,0 +1,48 @@
+package com.lin.kona.coroutine
+
+import kotlinx.coroutines.*
+import org.junit.Test
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class CoroutineTest {
+    @Test
+    fun testGlobalScope() {
+        GlobalScope.launch(Dispatchers.Default) {
+            println(233)
+        }
+    }
+
+    @Test
+    fun testFun() {
+        // funTest协程体
+        val funTest: suspend CoroutineScope.() -> Unit = {
+            println("funTest")
+            suspendFun1()
+            suspendFun2()
+        }
+        GlobalScope.launch(Dispatchers.Default, block = funTest)
+    }
+
+    // 挂起函数
+    private suspend fun suspendFun1() {
+        println("suspendFun1")
+    }
+
+    // 挂起函数
+    private suspend fun suspendFun2() {
+        println("suspendFun2")
+    }
+
+    @Test
+    fun testCoroutine() {
+        GlobalScope.launch {
+            println(233)
+            delay(1000)
+            println(23333)
+        }
+    }
+}
