0ef5c709f377bca2ab681fb7779ab358db0eaa72
fanhl <dyguests@gmail.com>
2023-03-12 12:47:35 +0800

    theme.

--- app/src/main/java/com/lin/kona/ui/MainActivity.kt
+++ app/src/main/java/com/lin/kona/ui/MainActivity.kt
@@ -2,15 +2,15 @@ package com.lin.kona.ui
 
 import android.os.Bundle
 import android.view.Menu
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.snackbar.Snackbar
 import com.lin.kona.R
 import com.lin.kona.databinding.ActivityMainBinding
 
@@ -21,6 +21,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
