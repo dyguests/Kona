1a3fe8afad2aa87839e404eb2b7f90f3a65316eb
dygue <dyguests@qq.com>
2024-11-15 15:26:13 +0800

    welcome main

--- auth/src/main/java/com/lin/kona/auth/WelcomeActivity.kt
+++ auth/src/main/java/com/lin/kona/auth/WelcomeActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.compose.setContent
 import com.lin.framework.base.BaseActivity
 import com.lin.kona.auth.screen.WelcomeScreen
 
+@Deprecated("废弃")
 class WelcomeActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
