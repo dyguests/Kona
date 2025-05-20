18efadca37655fd39ed78b5f8729ccb62da8d0e9
dyguests <dyguests@qq.com>
2025-05-20 15:34:47 +0800

    请求失败

--- bizKona/src/main/java/com/fanhl/kona/kona/repository/datasource/KonaDataSource.kt
+++ bizKona/src/main/java/com/fanhl/kona/kona/repository/datasource/KonaDataSource.kt
@@ -2,13 +2,29 @@ package com.fanhl.kona.kona.repository.datasource
 
 import com.fanhl.http.get
 import com.fanhl.kona.common.entity.Cover
+import com.fanhl.kona.kona.entity.KonaPost
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class KonaDataSource @Inject constructor() {
     suspend fun getPost(): List<Cover> {
-        return get("https://konachan.com/post.json")
-            .await<List<Cover>>()
+        // return get("https://konachan.com/post.json")
+        return get("/post.json")
+            .await<List<KonaPost>>()
+            .map { it.toCover() }
     }
+}
+
+private fun KonaPost.toCover(): Cover {
+    return Cover(
+        id = id?.toString(),
+        title = tags?.split(" ")?.firstOrNull(),
+        imageUrl = previewUrl,
+        description = tags,
+        author = author,
+        date = createdAt?.let { java.util.Date(it * 1000).toString() },
+        tags = tags?.split(" ")?.filter { it.isNotBlank() },
+        isFavorite = false
+    )
 }
--- libHttp/src/main/java/com/fanhl/http/HttpClient.kt
+++ libHttp/src/main/java/com/fanhl/http/HttpClient.kt
@@ -29,11 +29,11 @@ object HttpClient {
             .setDebug(true) // 开启调试模式
             // .setBaseUrl("https://api.example.com") // 设置基础URL
             .setConverter(GsonConverter.create())// 设置数据转换器
-            .setOnParamAssembly { param ->
-                // 在这里可以统一添加请求头等参数
-                param.addHeader("Content-Type", "application/json")
-                // 可以添加token等认证信息
-                // param.addHeader("Authorization", "Bearer ${getToken()}")
-            }
+            // .setOnParamAssembly { param ->
+            //     // 在这里可以统一添加请求头等参数
+            //     param.addHeader("Content-Type", "application/json")
+            //     // 可以添加token等认证信息
+            //     // param.addHeader("Authorization", "Bearer ${getToken()}")
+            // }
     }
 } 
