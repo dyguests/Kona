8673b43173b005087f7fb953e3074daa6934cb0d
dyguests <dyguests@qq.com>
2025-05-28 16:20:57 +0800

    SharedPreferenceProperty

--- libUtil/src/main/java/com/fanhl/util/GsonUtils.kt
+++ libUtil/src/main/java/com/fanhl/util/GsonUtils.kt
@@ -2,6 +2,7 @@ package com.fanhl.util
 
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import java.lang.reflect.Type
 
 object GsonUtils {
     val gson = Gson()
@@ -10,5 +11,56 @@ object GsonUtils {
         val json = gson.toJson(obj)
         val type = object : TypeToken<Map<String, Any?>>() {}.type
         return gson.fromJson(json, type)
+    }
+
+    /**
+     * Convert object to JSON string
+     */
+    fun <T> toJson(obj: T): String {
+        return gson.toJson(obj)
+    }
+
+    /**
+     * Convert JSON string to object
+     */
+    inline fun <reified T> fromJson(json: String): T? {
+        return try {
+            gson.fromJson(json, T::class.java)
+        } catch (e: Exception) {
+            null
+        }
+    }
+
+    /**
+     * Convert JSON string to object with Class type
+     */
+    fun <T> fromJson(json: String, type: Class<T>): T? {
+        return try {
+            gson.fromJson(json, type)
+        } catch (e: Exception) {
+            null
+        }
+    }
+
+    /**
+     * Convert JSON string to object with TypeToken
+     */
+    fun <T> fromJson(json: String, typeToken: TypeToken<T>): T? {
+        return try {
+            gson.fromJson(json, typeToken.type)
+        } catch (e: Exception) {
+            null
+        }
+    }
+
+    /**
+     * Convert JSON string to object with Type
+     */
+    fun <T> fromJson(json: String, type: Type): T? {
+        return try {
+            gson.fromJson(json, type)
+        } catch (e: Exception) {
+            null
+        }
     }
 } 
--- libUtil/src/main/java/com/fanhl/util/SharePreferenceExt.kt
+++ libUtil/src/main/java/com/fanhl/util/SharePreferenceExt.kt
@@ -1,5 +1,87 @@
 package com.fanhl.util
 
-import android.content.Context
-import android.content.SharedPreferences
-import androidx.core.content.edit
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
+
+/**
+ * Type-safe SharedPreferences property delegate
+ */
+class SharedPreferenceProperty<T>(
+    private val key: String,
+    private val defaultValue: T
+) : ReadWriteProperty<Any, T> {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun getValue(thisRef: Any, property: KProperty<*>): T {
+        return when (defaultValue) {
+            is String -> SharePreferenceUtil.getString(key, defaultValue) as T
+            is Int -> SharePreferenceUtil.getInt(key, defaultValue) as T
+            is Long -> SharePreferenceUtil.getLong(key, defaultValue) as T
+            is Float -> SharePreferenceUtil.getFloat(key, defaultValue) as T
+            is Boolean -> SharePreferenceUtil.getBoolean(key, defaultValue) as T
+            else -> {
+                val json = SharePreferenceUtil.getString(key, "")
+                if (json.isEmpty()) defaultValue
+                else GsonUtils.fromJson(json, defaultValue::class.java) ?: defaultValue
+            }
+        }
+    }
+
+    override fun setValue(thisRef: Any, property: KProperty<*>, value: T) {
+        when (value) {
+            is String -> SharePreferenceUtil.setString(key, value)
+            is Int -> SharePreferenceUtil.setInt(key, value)
+            is Long -> SharePreferenceUtil.setLong(key, value)
+            is Float -> SharePreferenceUtil.setFloat(key, value)
+            is Boolean -> SharePreferenceUtil.setBoolean(key, value)
+            else -> {
+                val json = GsonUtils.toJson(value)
+                SharePreferenceUtil.setString(key, json)
+            }
+        }
+    }
+
+    fun remove() {
+        SharePreferenceUtil.remove(key)
+    }
+
+    fun exists(): Boolean {
+        return SharePreferenceUtil.contains(key)
+    }
+}
+
+/**
+ * Creates a type-safe SharedPreferences property
+ */
+fun <T> sp(
+    key: String,
+    defaultValue: T
+): SharedPreferenceProperty<T> {
+    return SharedPreferenceProperty(key, defaultValue)
+}
+
+// Usage examples:
+/*
+class MyClass {
+    var myString by sp("KEY_STRING", "default")
+    var myInt by sp("KEY_INT", 0)
+    var myBoolean by sp("KEY_BOOLEAN", false)
+    var myUser by sp("KEY_USER", User("default", 0))
+    var myList by sp("KEY_LIST", listOf<User>())
+    
+    fun example() {
+        // Get value
+        val value = myString
+        
+        // Set value
+        myString = "new value"
+        myUser = User("John", 25)
+        
+        // Remove value
+        sp("KEY_STRING", "").remove()
+        
+        // Check if exists
+        val exists = sp("KEY_STRING", "").exists()
+    }
+}
+*/
